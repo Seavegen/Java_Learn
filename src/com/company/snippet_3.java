af22d726ee7d0a45bbc3a6b04d0ecad560633a4b
Dmitriy <q0pyo@yadnex.ru>
2020-11-12 14:19:04 +0300

    learn java

---
+++ src/com/company/snippet_3.java
@@ -0,0 +1,32 @@
+/*
+Даны три целых числа найти количество положительных чисел в исходном наборе
+ */
+
+package com.company;
+
+import java.util.*;
+
+public class snippet_3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Введите число для a: ");
+        int a = sc.nextInt();
+        System.out.print("Введите число для b: ");
+        int b = sc.nextInt();
+        System.out.print("Введите число для c: ");
+        int c = sc.nextInt();
+        double k = 0, ko = 0;
+
+        if (a > 0) k++;
+        else if (a < 0)ko++;
+        if (b > 0) k++;
+        else if (b < 0)ko++;
+        if (c > 0) k++;
+        else if (c < 0)ko++;
+
+        System.out.println("kp(>0) = "+k);
+        System.out.print("ko(<0) = "+ko);
+
+    }
+
+}
